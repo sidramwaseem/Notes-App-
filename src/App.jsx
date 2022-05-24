a5fcb883111e772a003adae0362bd8197079a3a7
sidramwaseem <sidraw24@gmail.com>
2022-05-24 19:36:01 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import NoteContainer from "./components/NoteContainer/NoteContainer";
+import Sidebar from "./components/Sidebar/Sidebar.jsx";
 
 export default function App() {
-  return(
+  return (
     <div className="App">
-    <NoteContainer />
+      <Sidebar />
+      <NoteContainer />
     </div>
   );
 }
