841179514dc661aa339c44aadc0364700347e283
sidramwaseem <sidraw24@gmail.com>
2022-05-24 19:36:58 +0500

    Update Sidebar.jsx

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -5,10 +5,16 @@ import plusIcon from "../../assets/plus-icon.png";
 function Sidebar() {
   const colours = ["#fe9b72", "#fec971", "#00d4fd", "#b693fd", "#e4ee91"];
 
+  const [listOpen, setListOpen] = React.useState(false);
+
+  function toggleList(){
+    setListOpen(prevState => !prevState)
+  }
+
   return (
     <div className="sidebar">
-      <img src={plusIcon} alt="Plus-Icon" />
-      <ul className="sidebar_list">
+      <img src={plusIcon} alt="Plus-Icon" onClick={toggleList} />
+      <ul className={`sidebar_list ${listOpen?"sidebar_list_active":""}`}>
         {colours.map((item, index) => (
           <li
             key={index}
