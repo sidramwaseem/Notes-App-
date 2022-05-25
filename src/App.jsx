7674ef263b76425436a1886211232ccadf93de2a
sidramwaseem <sidraw24@gmail.com>
2022-05-25 20:34:01 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,25 @@ import NoteContainer from "./components/NoteContainer/NoteContainer";
 import Sidebar from "./components/Sidebar/Sidebar.jsx";
 
 export default function App() {
+
+  const [notes, setNotes] = React.useState([])
+  
+  function addNote(color){
+    const tempNotes = [...notes]
+
+    tempNotes.push({
+      id: Date.now()+""+Math.floor(Math.random()*78) ,
+      text:"",
+      time:Date.now(),
+      color:color
+    })
+    setNotes(tempNotes);
+  }
+
   return (
     <div className="App">
-      <Sidebar />
-      <NoteContainer />
+      <Sidebar addNote={addNote} />
+      <NoteContainer notes={notes} />
     </div>
   );
 }
