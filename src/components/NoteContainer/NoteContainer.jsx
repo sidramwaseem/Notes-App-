ef0c8061104f246d7dc008858d814f36adfdbcfc
sidramwaseem <sidraw24@gmail.com>
2022-05-25 20:34:17 +0500

    Update NoteContainer.jsx

--- src/components/NoteContainer/NoteContainer.jsx
+++ src/components/NoteContainer/NoteContainer.jsx
@@ -2,60 +2,25 @@ import React from "react";
 import Note from "../Notes/Note.jsx";
 import "./NoteContainer.css";
 
-export default function NoteContainer() {
+export default function NoteContainer(props) {
+  const reverseArray = (arr) => {
+    const array = [];
+
+    for (let i = arr.length - 1; i >= 0; --i) {
+      array.push(arr[i]);
+    }
+    return array;
+  };
+  const notes = reverseArray(props.notes);
   return (
     <div className="note-container">
       <h3>Notes</h3>
       <div className="note-container_notes custom-scroll">
-        <Note
-          note={{
-            text: "Whatever",
-            time: "4:32PM",
-            color: "cyan",
-          }}
-        />
-        <Note
-          note={{
-            text: "Whatever",
-            time: "4:32PM",
-            color: "cyan",
-          }}
-        />
-        <Note
-          note={{
-            text: "Whatever",
-            time: "4:32PM",
-            color: "cyan",
-          }}
-        />
-        <Note
-          note={{
-            text: "Whatever",
-            time: "4:32PM",
-            color: "cyan",
-          }}
-        />
-        <Note
-        note={{
-          text: "Whatever",
-          time: "4:32PM",
-          color: "cyan",
-        }}
-      />
-      <Note
-      note={{
-        text: "Whatever",
-        time: "4:32PM",
-        color: "cyan",
-      }}
-    />
-    <Note
-    note={{
-      text: "Whatever",
-      time: "4:32PM",
-      color: "cyan",
-    }}
-  />
+        {notes?.length > 0 ? (
+          notes.map((item) => <Note key={item.id} note={item} />)
+        ) : (
+          <h3>No Notes present</h3>
+        )}
       </div>
     </div>
   );
