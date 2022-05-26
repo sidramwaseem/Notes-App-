112e6621a831264833c3d6548af579110e3a1488
sidramwaseem <sidraw24@gmail.com>
2022-05-26 13:07:47 +0500

    Updated NoteContainer.jsx

--- src/components/NoteContainer/NoteContainer.jsx
+++ src/components/NoteContainer/NoteContainer.jsx
@@ -12,14 +12,17 @@ export default function NoteContainer(props) {
     return array;
   };
   const notes = reverseArray(props.notes);
+
   return (
     <div className="note-container">
-      <h3>Notes</h3>
+      <h3>My Notes</h3>
       <div className="note-container_notes custom-scroll">
         {notes?.length > 0 ? (
-          notes.map((item) => <Note key={item.id} note={item} />)
+          notes.map((item) => <Note key={item.id} note={item}
+          deleteNote={props.deleteNote} 
+          updateText={props.updateText} />)
         ) : (
-          <h3>No Notes present</h3>
+          <h3 className="note-container_noNotes-text">No Notes present</h3>
         )}
       </div>
     </div>
