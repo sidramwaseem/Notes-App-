a614b213863a1e1385a532e04a8d27309f3e9517
sidramwaseem <sidraw24@gmail.com>
2022-05-26 12:10:31 +0500

    Added delete icon

--- src/components/Notes/Note.jsx
+++ src/components/Notes/Note.jsx
@@ -1,11 +1,15 @@
 import React from "react";
+import deleteIcon from "../../assets/delete-icon.png"
 import "./Note.css";
 
 function Note(props) {
   return (
     <div className="note" style={{ backgroundColor: props.note.color }}>
       <textarea className="note_text" defaultValue={props.note.text} />
-      <p>{props.note.time}</p>
+      <div className="note_footer">
+        <p>{props.note.time}</p>
+        <img src={deleteIcon} alt="delete-icon"/>
+      </div>
     </div>
   );
 }
